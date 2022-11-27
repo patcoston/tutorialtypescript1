bdfe9e5c1cafa3212c050a5cb5e737b1d1de1189
Patrick Coston <patcoston@gmail.com>
2022-11-27 17:35:58 -0500

    NoteList added code for Title and Tags

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,37 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import ReactSelect from 'react-select'
+import { Tag } from './App'
 
-export class NoteList extends Component {
-  render() {
-    return (
-      <>
-        <Row>
+type NoteListProps = {
+  availableTags: Tag[]
+}
+
+const NoteList = ({ availableTags }: NoteListProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
           <Col>
-            <h1>Notes</h1>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
           </Col>
-          <Col xs="auto">
-            <Stack gap={2} direction="horizontal">
-              <Link to="/new">
-                <Button variant="primary">Create</Button>
-              </Link>
-              <Button variant="outline-secondary">Edit Tags</Button>
-            </Stack>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags?.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                options={availableTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                onChange={tags => {
+                  setSelectedTags(
+                    tags.map(tag => {
+                      return { label: tag.label, id: tag.value }
+                    }),
+                  )
+                }}
+                isMulti
+              />
+            </Form.Group>
           </Col>
         </Row>
-        <Form>
-          <Row className="mb-4">
-            <Col>
-              <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-            </Col>
-          </Row>
-        </Form>
-      </>
-    )
-  }
+      </Form>
+    </>
+  )
 }
 
 export default NoteList
