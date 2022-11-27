b393b306195dcf54e438532ecd4afdb308bf8dff
Patrick Coston <patcoston@gmail.com>
2022-11-27 15:11:59 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,19 @@
+c:\web
+npm create vite
+> Project name: tutorialts1
+> React
+> TypeScript
+cd tutorialts1
+npm install
+npm run dev
+npm i react-bootstrap bootstrap react-router-dom react-select
+npm i --save-dev @types/uuid
+
+
+
+App.tsx
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+Snippets
+https://github.com/ults-io/vscode-react-javascript-snippets/blob/HEAD/docs/Snippets.md
+
---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import { useMemo } from 'react'
+import { Container } from 'react-bootstrap'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import NewNote from './NewNote'
+import useLocalStorage from './useLocalStorage'
+import { v4 as uuidV4 } from 'uuid'
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+const App = () => {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
+  const addTag = (tag: Tag) => {
+    setTags(prev => [...prev, tag])
+  }
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {
+        ...note,
+        tags: tags.filter(tag => note.tagIds.includes(tag.id)),
+      }
+    })
+  }, [notes, tags])
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route
+            index
+            path="edit"
+            element={
+              <NewNote
+                onSubmit={onCreateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
