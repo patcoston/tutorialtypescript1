f280a46a39887ae50fbaf1912db6f27a6aff45f4
Patrick Coston <patcoston@gmail.com>
2022-11-27 17:09:42 -0500

    Added Row Col Stack Button to NoteList

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
+import { Button, Col, Row, Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export class NoteList extends Component {
   render() {
-    return <div>NoteList</div>
+    return (
+      <>
+        <Row>
+          <Col>
+            <h1>Notes</h1>
+          </Col>
+          <Col xs="auto">
+            <Stack gap={2} direction="horizontal">
+              <Link to="/new">
+                <Button variant="primary">Create</Button>
+              </Link>
+            </Stack>
+          </Col>
+        </Row>
+      </>
+    )
   }
 }
 
