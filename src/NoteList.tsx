fa9c6cf16084db481736ea2e8dfa7b888fbbdcd5
Patrick Coston <patcoston@gmail.com>
2022-11-28 12:28:42 -0500

    Added CSS module for NoteList

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,8 +1,9 @@
 import React, { Component, useMemo, useState } from 'react'
-import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
+import { Button, Card, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import ReactSelect from 'react-select'
-import { Note, Tag } from './App'
+import { Tag } from './App'
+import styles from './NoteList.module.css'
 
 type SimplifiedNote = {
   tags: Tag[]
@@ -94,7 +95,15 @@ const NoteList = ({ availableTags, notes }: NoteListProps) => {
 }
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
-  return <h1>HI</h1>
+  return (
+    <Card
+      as={Link}
+      to={'/${id'}
+      className={`h-100 text-reset text-decoration-note ${styles.card}`}
+    >
+      <Card.Body></Card.Body>
+    </Card>
+  )
 }
 
 export default NoteList
